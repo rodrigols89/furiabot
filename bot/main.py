bd1c09be15ca9af7d05ad77bd566ac1f5828602d
rodrigols89 <rodrigols89.inbox@gmail.com>
2025-05-01 10:24:49 -0300

    2.4. Instalar dependências básicas e testar no Telegram

---
+++ bot/main.py
@@ -0,0 +1,26 @@
+import os
+from dotenv import load_dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+# Carrega o .env
+load_dotenv()
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+# Função simples para responder ao comando /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f"Olá, fã da FURIA! 🔥")
+
+# Inicia o bot
+def main():
+    if not TELEGRAM_TOKEN:
+        raise ValueError("TELEGRAM_TOKEN não encontrado no arquivo .env")
+
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+
+    print("🤖 Bot rodando... Envie /start no Telegram.")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
